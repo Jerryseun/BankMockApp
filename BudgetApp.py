5f72dd015e37770dd78d04a5f5b6422a7394d2d3
Jerryseun <Jerryseun@users.noreply.github.com>
2021-05-04 23:47:20 +0100

    first commit

---
+++ BudgetApp.py
@@ -0,0 +1,14 @@
+class BudgetCategory:
+
+    def __init__(self, name):
+        self.name = name
+
+
+    def deposit_fund(self):
+        deposit_amount = int(input("Enter you want to deposit: "))
+
+
+    def withdraw_fund(self):
+
+
+    def transfer_fund(self):
